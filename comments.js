26cdf74dd873d7e9b4ac50b2e53a5364a3b15a65
Lu <denloh98@gmail.com>
2023-11-14 14:19:15 +0000

    Copilot third commt

---
+++ comments.js
@@ -0,0 +1,30 @@
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:3000
+// Note: Ctrl+C to stop server
+
+var http = require('http');
+var url = require('url');
+var items = [];
+var server = http.createServer(function(req, res) {
+  switch(req.method) {
+    case 'POST':
+      var item = '';
+      req.setEncoding('utf8');
+      req.on('data', function(chunk) {
+        item += chunk;
+      });
+      req.on('end', function() {
+        items.push(item);
+        res.end('OK\n');
+      });
+      break;
+    case 'GET':
+      items.forEach(function(item, i) {
+        res.write(i + ') ' + item + '\n');
+      });
+      res.end();
+      break;
+  }
+});
+server.listen(3000);
